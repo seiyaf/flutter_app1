0bb050acb1638143973990c5166e566c3290fd66
seiya fukushima <seiyafukusima@gmail.com>
2021-08-12 14:37:04 +0900

    Initial commit

---
+++ test/widget_test.dart
@@ -0,0 +1,23 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_app1/main.dart';
+
+void main() {
+  testWidgets('flutte_app sample test', (WidgetTester tester) async {
+
+    await tester.pumpWidget(new MyApp());
+    await tester.pump();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.byIcon(Icons.add),findsOneWidget);
+  });
+}
